cb8d8a5b5ff204d878dfe50a3d01868113aeba83
Drew André <dandre@conncoll.edu>
2017-11-12 17:07:03 -0500

    add core-js updates

--- app/javascript/react/test/testHelper.js
+++ app/javascript/react/test/testHelper.js
@@ -4,9 +4,9 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import jasmineEnzyme from 'jasmine-enzyme';
 import Enzyme from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-16';
 
-// Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
 
 beforeEach(() => {
   jasmineEnzyme();
