af914602ed3cd302ae6dec64bd099d16729790f5
Drew André <dandre@conncoll.edu>
2017-12-04 11:26:20 -0500

    add product save validation

--- spec/controllers/product_controller_spec.rb
+++ spec/controllers/product_controller_spec.rb
@@ -3,9 +3,8 @@ require 'rails_helper'
 RSpec.describe ProductsController, type: :controller do
   describe "new product" do
     let(:product) { create(:product) }
-    # it "should create a new product" do
-    #   get :new
-    #   expect(Product).to be_instance_of(Product)
-    # end
+    it "should create a new product" do
+      expect(product).to be_valid
+    end
   end
 end
